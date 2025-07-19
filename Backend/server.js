702ef4e1e4195f9d671d4378f143f2ac2b7b35d7
ShivanshSol <shivanshsolanki2003@gmail.com>
2025-07-19 17:15:52 +0530

    Updated backend with unified file structure, addedheader and footer with ejs for modular approach

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const env = require('dotenv');
 
+// Load .env config
+env.config();
+
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// View engine setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Routes
 app.get('/', (req, res) => {
-  res.send('Success!');
+  res.render('landing');
+});
+app.get('/home', (req, res) => {
+  res.render('home');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
